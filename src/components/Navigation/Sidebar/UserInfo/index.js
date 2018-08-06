adb065878f310f205bbb4dc420b05694ac51df20
o.kostyannikova <o.kostyannikova@gmail.com>
2018-08-06 11:19:01 +0300

    PROP-58: added progress bar

--- src/components/Navigation/Sidebar/UserInfo/index.js
+++ src/components/Navigation/Sidebar/UserInfo/index.js
@@ -17,7 +17,17 @@ export default class UserInfo extends Component {
           <div className="user-info__avatar-wrapper">
             <img src={user.avatar} alt="User avatar" />
           </div>
-          <span className="user-info__progress">{user.progress}</span>
+          <svg>
+            <circle
+              className="user-info__progress-bar"
+              r="36"
+              cx="50%"
+              cy="50%"
+              strokeDashoffset={-237 - user.progress * 2.37}
+              transform="rotate(-90 38 38)"
+            />
+          </svg>
+          <span className="user-info__progress-nummber">{user.progress}</span>
         </div>
         <p className="user-info__name">{user.name}</p>
         <p className="user-info__email">{user.email}</p>
