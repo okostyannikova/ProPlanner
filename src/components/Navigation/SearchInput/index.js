60d49260057aa9214fdfae176144d155550e3801
Dmitry Ivanov <dmitry.ivanov.od@gmail.com>
2018-09-15 23:54:17 +0300

    move search icon to Icons folder

--- src/components/Navigation/SearchInput/index.js
+++ src/components/Navigation/SearchInput/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import SearchIcon from './SearchIcon';
+import SearchIcon from '../../Icons/SearchIcon';
 
 const SearchInput = () => (
   <div className="search">
