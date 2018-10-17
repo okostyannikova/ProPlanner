468d03190124625b4b78c4cab79a72db8136337c
Gangrels <antlukyanenko@gmail.com>
2018-10-17 13:42:06 +0300

    PROP-247: Add timepicker validation

--- src/components/TimePickerComponent/Time/timeField.js
+++ src/components/TimePickerComponent/Time/timeField.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import * as moment from 'moment';
 import { InlineDateTimePicker } from 'material-ui-pickers/DateTimePicker';
 
+const styles = {
+  timeField: {
+    borderBottom: '2px solid transparent',
+    '&:hover': {
+      borderBottom: '2px solid #00BCD4',
+    },
+  },
+  timeFieldError: {
+    borderBottom: '2px solid red',
+  },
+};
+
 export default class timeField extends Component {
   state = { selectedDate: moment().format() };
 
   componentWillReceiveProps(newProps) {
+    // console.log(newProps);
+    // console.log(this.props);
+    const { meta } = newProps;
+    const { validationHandler } = this.props;
+    // meta.error && validationHandler(meta);
+    validationHandler && validationHandler(meta);
     this.setState({ selectedDate: newProps.input.value });
   }
 
@@ -17,8 +35,12 @@ export default class timeField extends Component {
 
   render() {
     const { selectedDate } = this.state;
-    const { view } = this.props;
+    const { view, selectStartDate, maxDate, maxDateMessage, error } = this.props;
 
+    // console.log('11', error);
+    // // selectStartDate(selectedDate);
+    selectStartDate && selectStartDate(selectedDate);
+    console.log(styles);
     return (
       <span>
         {view ? (
@@ -34,6 +56,13 @@ export default class timeField extends Component {
             onChange={this.handleDateChange}
             ampm={false}
             format="D MMM YYYY  HH:mm"
+            maxDate={maxDate}
+            maxDateMessage={maxDateMessage}
+            className="qqqq"
+            style={{
+              ...styles.timeField,
+              ...(error ? styles.timeFieldError : styles.timeField),
+            }}
           />
         )}
       </span>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,3 +1,5 @@
+import * as moment from 'moment';
+
 export const required = value => (value ? undefined : 'Value is required');
 
 export const maxTitleLength = value =>
@@ -5,3 +7,8 @@ export const maxTitleLength = value =>
 
 export const maxDescriptionLength = value =>
   value.length > 255 ? 'Value is too long (Max 255 symbols)' : undefined;
+
+export const timeValidation = (value, allValues) =>
+  moment(value).diff(moment(allValues.startTime), 'minutes') < 30
+    ? 'Minimum distance between start time/end time must be 30 minutes'
+    : undefined;
